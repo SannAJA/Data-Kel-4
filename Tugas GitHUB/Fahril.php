6b1157b54a3db8b2c1ed5207869dcf6c3d425c8e
SannAJA <ihsanadzki@gmail.com>
2024-11-02 22:55:09 +0700

    Add files via upload

---
+++ Tugas GitHUB/Fahril.php
@@ -0,0 +1,15 @@
+<?php
+//variabel
+$nama="muhammad fahril";
+echo "nama saya adalah $nama<br>";
+
+//constanta
+define("UMUR","15");
+echo "Umur saya : ",umur."tahun<br>;
+
+//tipe data
+sttl =['06-11-2008,','serua bulak,pondokpetir,depok'];
+echo"tempat tanggal lahir : ",implode(sttl);
+
+
+?>
