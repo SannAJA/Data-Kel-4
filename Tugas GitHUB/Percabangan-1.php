f1b4160cc42a3ad5bba99226531ec2983de5c958
SannAJA <ihsanadzki@gmail.com>
2024-11-02 22:27:51 +0700

    Add files via upload

---
+++ Tugas GitHUB/Percabangan-1.php
@@ -0,0 +1,14 @@
+<?php
+//input nilai A dan B
+
+$B="61";
+
+$A="6";
+
+//Bandingkan nilai A dan B
+if ($A > $B) {
+echo "Nilai terbesar Adalah A: $A;
+} elseif ($A < $B) {
+echo "NIlai terbesar adalah B: $B;
+} else {
+echo "Kedua nilai sama: . $A;}?>
